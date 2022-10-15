ece6b3a7563041cd2b15bf1eddac2c188e443fd9
Vitaliy <fireflier87@gmail.com>
2022-10-15 23:01:00 +0300

    step 1.1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,34 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import FeedbackOptions from './FeedbackOptions';
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  onLeaveFeedback = e => {
+    const vote = e.target.vote;
+    this.setState(prevState => ({ [vote]: prevState[vote] + 1 }));
+  };
+
+  render() {
+    const options = Object.keys(this.state);
+    // const { good, neutral, bad } = this.state;
+
+    return (
+      <div>
+        <h2>Please leave feedback</h2>
+        <FeedbackOptions
+          options={options}
+          onLeaveFeedback={this.onLeaveFeedback}
+        />
+        <h2>Statistics</h2>
+        <ul></ul>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types';
+
+const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+  return (
+    <ul>
+      {options.map((option, idx) => (
+        <li key={idx}>
+          <button type="button" name={option} onClick={onLeaveFeedback}>
+            {option}
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+FeedbackOptions.propTypes = {
+  options: PropTypes.array.isRequired,
+  onLeaveFeedback: PropTypes.func.isRequired,
+};
+
+export default FeedbackOptions;
