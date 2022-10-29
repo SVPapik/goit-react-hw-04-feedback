41d91d7b41eb979b370dc4670fc71a4320c180fb
Vitaliy <fireflier87@gmail.com>
2022-10-29 19:23:14 +0300

    refactor

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import FeedbackOptions from './FeedbackOptions';
 import Section from './Section';
 import Statistics from './Statistics';
 import Notification from './Notification';
+// import React from 'react';
 
 const App = () => {
   const [state, setState] = useState({ good: 0, neutral: 0, bad: 0 });
