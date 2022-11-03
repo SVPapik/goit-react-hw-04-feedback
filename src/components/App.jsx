15e46c2507e8f9bcaf5d99fc2bf93d9029aab1f0
Vitaliy <fireflier87@gmail.com>
2022-11-04 00:29:15 +0200

    refactored Total & percentage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,23 +6,20 @@ import Notification from './Notification';
 
 const App = () => {
   const [state, setState] = useState({ good: 0, neutral: 0, bad: 0 });
+  const [totalVotes, setTotalVotes] = useState({ total: 0 });
+  const [rate, setRate] = useState({ percentage: 0 });
 
   const onLeaveFeedback = option => {
     setState(prevState => ({ ...prevState, [option]: prevState[option] + 1 }));
-  };
-
-  const countTotalFeedback = () => {
-    return Object.values(state).reduce((total, item) => total + item);
-  };
-
-  const countPositiveFeedbackPercentage = () => {
-    return +Math.round((state.good / countTotalFeedback()) * 100);
+    setTotalVotes(() => ({ total: total + 1 }));
+    const percent = +Math.round((good / total) * 100);
+    setRate(() => ({ percentage: percent }));
   };
 
   const options = Object.keys(state);
   const { good, neutral, bad } = state;
-  const total = countTotalFeedback();
-  const positiveFeedback = countPositiveFeedbackPercentage();
+  const { total } = totalVotes;
+  const { percentage } = rate;
   return (
     <div className="widget-container">
       <Section title="Please leave feedback">
@@ -35,7 +32,7 @@ const App = () => {
             neutral={neutral}
             bad={bad}
             total={total}
-            positivePercentage={positiveFeedback}
+            positivePercentage={percentage}
           />
         ) : (
           <Notification message="There is no feedback"></Notification>
